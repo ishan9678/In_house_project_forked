19142d08d6be6cf1dd301f930f5d7613f7203ec7
ishan <is9678@srmist.edu.in>
2023-09-16 20:49:39 +0530

    dashboard css

--- app/components/dashboard/dashboard.jsx
+++ app/components/dashboard/dashboard.jsx
@@ -1,57 +1,61 @@
-"use client"
-import React, { useState, useEffect } from "react";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
 
-import Leaverequest from '../leaverequestform/Requestform'
+import Leaverequest from "../leaverequestform/Requestform";
 import { redirect } from "next/navigation";
-import Leavestatus from '../leavestatus/leavestatus'
+import Leavestatus from "../leavestatus/leavestatus";
 import { useSession } from "next-auth/react";
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import ".//Navbar.css";
 
-export default function Dashboard(){
+export default function Dashboard() {
   const { data: session } = useSession();
   if (!session) redirect("/");
-  const regnum= session?.user?._doc.regnum;
-  const role=session?.user?._doc.role;
+  const regnum = session?.user?._doc.regnum;
+  const role = session?.user?._doc.role;
 
-  
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    if (dropdownRef.current) {
+      dropdownRef.current.style.display =
+        dropdownRef.current.style.display === "block" ? "none" : "block";
+    }
+  };
+
   return (
-
     <div>
-<nav className="navbar navbar-expand-lg navbar-dark nav-color">
-          <redirect className="navbar-brand" to="/dashboard">
-           
-          </redirect>
-          <img src="" className="logo md-1" alt="SRMIST" />
-
-          <h2>Welcome to application {name}</h2>
-          
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-link">
-              <redirect href="">
-                <i className="fa-solid fa-user"></i>
-              </redirect >
-              <div className="flex-box">
-                <span className="reg">{regnum}(Student)</span>
-                <button onClick={e => logout(e)} className="btn btn-primary">
-            Logout
-          </button>
-              </div>
-            </li>
-          </ul>
-        </nav>
-        <h3 className="head"> Leave Application</h3>
-        <div className="nithin">
-
+      <div className="navbar">
+        <img src="" className="logo" alt="SRMIST" />
+        <div className="profile">
+          <img
+            src="profile-image-url.jpg"
+            className="profile-image"
+            alt="Profile"
+          />
+          <div className="nav-item dropdown">
+            <a href="/profile" className="dropdown-toggle regnum">
+              {regnum} (Student)
+            </a>
+            <ul className="nav-dropdown" ref={dropdownRef}>
+              <li>
+                <a href="/profile">Profile</a>
+              </li>
+              <li>
+                <span onClick={(e) => logout(e)}>Logout</span>
+              </li>
+            </ul>
+          </div>
         </div>
-
-     
-
+      </div>
+      <div className="buttons">
+        <button className="button">Leave Application</button>
+        <button className="button">Leave Request</button>
+      </div>
     </div>
   );
-};
-
-
+}
